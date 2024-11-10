4340aaa7955a0ed1f431b060febd68e249c0aeeb
TungPT12 <mylifeundermycontrol@gmail.com>
2024-11-10 15:32:12 +0700

    input base ui

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,31 @@
-import { FC, PropsWithChildren } from 'react'
+import { FC } from 'react'
+import styles from './Input.module.css'
+import PasswordImage from '@/assets/images/svgs/passowrd_icon.svg';
+interface InputProps {
+  type: string
+  value: string
+  placeholder: string
+  handleValidation: () => void
+  onChange: () => void,
+  className: string
+}
 
-export const Input: FC<PropsWithChildren> = () => {
-  return <div className='input'>Input</div>
+export const Input: FC<InputProps> = (props) => {
+  const {
+    type = 'text',
+    value = "",
+    placeholder = '',
+    handleValidation = () => {},
+    onChange = () => {},
+    className = ""
+  } = props;
+
+
+  return (
+    <div className={`${styles['input__wrapper']} ${className}`}>
+      <PasswordImage className='h-3.5 w-auto'/>
+      <input className={`${styles["input"]}`} onChange={onChange}  type={type}/>
+      <div className={`${styles['placeholder']}`}>{placeholder}</div>
+    </div>
+  )
 }
