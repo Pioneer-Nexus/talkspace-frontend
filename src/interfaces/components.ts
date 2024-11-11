6bc2df53eba7118f2beb59c8345467feabf6e5a5
Noah <phvohieunghia@gmail.com>
2024-11-11 19:46:00 +0700

    TS-32 finish type, icon, size

---
+++ src/interfaces/components.ts
@@ -0,0 +1,17 @@
+export enum ButtonType {
+  PRIMARY = 'primary',
+  DEFAULT = 'default',
+  TEXT = 'text',
+  LINK = 'link',
+}
+
+export enum ButtonSize {
+  LARGE = 'large',
+  DEFAULT = 'default',
+  SMALL = 'small',
+}
+
+export enum ButtonIconPosition {
+  START = 'start',
+  END = 'end',
+}
