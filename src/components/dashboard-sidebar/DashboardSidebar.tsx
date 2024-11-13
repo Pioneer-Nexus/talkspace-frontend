cc94cbfb3ce2e9606ffbcd7e1aca1771f0d3d637
Noah <phvohieunghia@gmail.com>
2024-11-13 19:47:12 +0700

    TS-33 create sidebar component

---
+++ src/components/dashboard-sidebar/DashboardSidebar.tsx
@@ -0,0 +1,10 @@
+import clsx from 'clsx'
+
+export const DashboardSidebar = () => {
+  return (
+    <div className={clsx('flex flex-col')}>
+      <div className={''}></div>
+      <div className={''}></div>
+    </div>
+  )
+}
